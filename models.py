d663be7c01f0706f40393d96db1800a34f12ff39
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-09 23:58:43 +0300

    revised capsule model

--- models.py
+++ models.py
@@ -23,41 +23,41 @@ class MultiGPUNet(models.Model):
 def FashionCapsNet(input_shape, n_class, args):
     x = layers.Input(shape=input_shape)
 
+    def residual_block(y, nb_channels, _strides=(2, 2), _project_shortcut=False):
+        shortcut = y
+
+        # down-sampling is performed with a stride of 2
+        y = layers.Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+        y = layers.BatchNormalization()(y)
+        y = layers.LeakyReLU()(y)
+
+        y = layers.Conv2D(nb_channels, kernel_size=(3, 3), strides=(1, 1), padding='same')(y)
+        y = layers.BatchNormalization()(y)
+
+        # identity shortcuts used directly when the input and output are of the same dimensions
+        if _project_shortcut or _strides != (1, 1):
+            # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
+            # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
+            shortcut = layers.Conv2D(nb_channels, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+            shortcut = layers.BatchNormalization()(shortcut)
+
+        y = layers.add([shortcut, y])
+        y = layers.LeakyReLU()(y)
+
+        return y
+
     # Stacked-convolutional layer 1
-    out = layers.Conv2D(filters=int(args.conv_filters/4),
-                        kernel_size=args.conv_kernel_size,
-                        strides=1,
-                        padding='valid',
-                        name='conv1')(x)
+    out = layers.Conv2D(filters=32, kernel_size=7, strides=1, padding='same', name='conv1_1')(x)
     out = layers.BatchNormalization()(out)
     out = layers.LeakyReLU()(out)
+    out = layers.SpatialDropout2D(rate=0.3)(out)
 
-    # Stacked-convolutional layer 2
-    out = layers.Conv2D(filters=int(args.conv_filters/2),
-                        kernel_size=args.conv_kernel_size,
-                        strides=2,
-                        padding='valid',
-                        name='conv2')(out)
-    out = layers.BatchNormalization()(out)
-    out = layers.LeakyReLU()(out)
-
-    # Stacked-convolutional layer 3
-    out = layers.Conv2D(filters=args.conv_filters,
-                        kernel_size=args.conv_kernel_size,
-                        strides=2,
-                        padding='valid',
-                        name='conv3')(out)
-    out = layers.BatchNormalization()(out)
-    out = layers.LeakyReLU()(out)
-
-    # Stacked-convolutional layer 4
-    out = layers.Conv2D(filters=args.conv_filters,
-                        kernel_size=args.conv_kernel_size,
-                        strides=2,
-                        padding='valid',
-                        name='conv4')(out)
-    out = layers.BatchNormalization()(out)
-    out = layers.LeakyReLU()(out)
+    out = residual_block(out, nb_channels=64, _project_shortcut=True)
+    out = layers.SpatialDropout2D(rate=0.3)(out)
+    out = residual_block(out, nb_channels=128, _project_shortcut=True)
+    out = layers.SpatialDropout2D(rate=0.3)(out)
+    out = residual_block(out, nb_channels=256, _project_shortcut=True)
+    out = layers.SpatialDropout2D(rate=0.3)(out)
 
     # PrimaryCaps
     out = PrimaryCaps(out, dim_capsule=args.dim_capsule)
@@ -78,19 +78,19 @@ def FashionCapsNet(input_shape, n_class, args):
     decoder = models.Sequential(name='decoder')
     decoder.add(layers.Dense(7*7*args.conv_filters, activation='relu', input_dim=args.dim_capsule*n_class))
     decoder.add(layers.Reshape((7, 7, args.conv_filters)))
-    decoder.add(layers.Conv2DTranspose(args.conv_filters, kernel_size=args.conv_kernel_size, strides=2, padding='same'))
+    decoder.add(layers.Conv2DTranspose(args.conv_filters, kernel_size=4, strides=2, padding='same'))
     decoder.add(layers.BatchNormalization(axis=-1))
     decoder.add(layers.LeakyReLU())
-    decoder.add(layers.Conv2DTranspose(int(args.conv_filters/2), kernel_size=args.conv_kernel_size, strides=2, padding='same'))
+    decoder.add(layers.Conv2DTranspose(int(args.conv_filters/2), kernel_size=4, strides=2, padding='same'))
     decoder.add(layers.BatchNormalization(axis=-1))
     decoder.add(layers.LeakyReLU())
-    decoder.add(layers.Conv2DTranspose(int(args.conv_filters / 4), kernel_size=args.conv_kernel_size, strides=2, padding='same'))
+    decoder.add(layers.Conv2DTranspose(int(args.conv_filters / 4), kernel_size=4, strides=2, padding='same'))
     decoder.add(layers.BatchNormalization(axis=-1))
     decoder.add(layers.LeakyReLU())
-    decoder.add(layers.Conv2DTranspose(int(args.conv_filters / 8), kernel_size=args.conv_kernel_size, strides=2, padding='same'))
+    decoder.add(layers.Conv2DTranspose(int(args.conv_filters / 8), kernel_size=4, strides=2, padding='same'))
     decoder.add(layers.BatchNormalization(axis=-1))
     decoder.add(layers.LeakyReLU())
-    decoder.add(layers.Conv2DTranspose(3, kernel_size=9, strides=2, padding='same', activation='sigmoid'))
+    decoder.add(layers.Conv2DTranspose(3, kernel_size=4, strides=2, padding='same', activation='sigmoid'))
 
     # Model for training
     train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
